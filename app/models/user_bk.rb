7e4ae92ff9b3332bde09c6dbea099fed8dc07d92
今村真人 <imamuramasato@imamuramasato-no-MacBook-Air.local>
2017-08-26 19:48:38 +0900

    3

--- app/models/user_bk.rb
+++ app/models/user_bk.rb
@@ -4,21 +4,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :trackable, :validatable,:confirmable,:omniauthable
 
-  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploader #deviseの設定配下に追記
 
   has_many :blogs
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.find_by(email: auth.info.email)
-
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
     unless user
       user = User.new(
-          name:     auth.extra.raw_info.name,
-          provider: auth.provider,
-          uid:      auth.uid,
-          email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-          image_url:   auth.info.image,
-          password: Devise.friendly_token[0, 20]
+        name:     auth.extra.raw_info.name,
+        provider: auth.provider,
+        uid:      auth.uid,
+        email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+        image_url:   auth.info.image,
+        password: Devise.friendly_token[0, 20]
       )
       user.skip_confirmation!
       user.save(validate: false)
@@ -26,11 +25,10 @@ class User < ActiveRecord::Base
     user
   end
 
-
-    def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
 
-      unless user
+    unless user
       user = User.new(
           name:     auth.info.nickname,
           image_url: auth.info.image,
@@ -41,21 +39,21 @@ class User < ActiveRecord::Base
       )
       user.skip_confirmation!
       user.save
-      end
-      user
     end
+    user
+  end
 
-def self.create_unique_string
-    SecureRandom.uuid
-end
+  def self.create_unique_string
+      SecureRandom.uuid
+  end
 
-def update_with_password(params, *options)
-    if provider.blank?
-      super
+  def update_with_password(params, *options)
+      if provider.blank?
+        super
       else
-      params.delete :current_password
-      update_without_password(params, *options)
-    end
+        params.delete :current_password
+        update_without_password(params, *options)
+      end
+  end
 
-end
 end
