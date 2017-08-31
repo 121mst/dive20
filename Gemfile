c3a0499e6ac629adff47299db4f976cf708b44a8
今村真人 <imamuramasato@imamuramasato-no-MacBook-Air.local>
2017-08-31 22:44:43 +0900

    init unicorn

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'unicorn'
 gem 'jquery-turbolinks'
 
 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server '13.114.207.179', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/imamuramasato/.ssh/id_rsa'
