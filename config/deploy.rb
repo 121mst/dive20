73d4d86323608297fb68589c6cd7a5aac2d33f30
今村真人 <imamuramasato@imamuramasato-no-MacBook-Air.local>
2017-08-31 22:59:35 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'dive20'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/121mst/dive20.git'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/121mst/dive20.git'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/dive20'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
